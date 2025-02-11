225bddb2e61e233ab62faf71137d98a675aed875
eric12345678910 <ericlaudrum@gmail.com>
2025-02-11 16:32:42 -0500

    Update Inventory.cs to resolve AddProducts method

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -2,7 +2,14 @@ namespace COMP2139_Assignment1.Models;
 
 public class Inventory
 {
-    private List<Product> products;
+    // Attributes
+    private List<Product> _products = new List<Product>();
 
+    // Methods
+    // Add product to inventory
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
 
 }
