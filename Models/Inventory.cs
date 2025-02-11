889b0a0616bf7e5bbee629e500b8a9cc8b628206
eric12345678910 <ericlaudrum@gmail.com>
2025-02-11 15:39:53 -0500

    Move product attributes from Inventory to Products and create list of Products in Inventory.

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -2,10 +2,10 @@ namespace COMP2139_Assignment1.Models;
 
 public class Inventory
 {
-    public int ProductId { get; set; }
-    public string ProductName { get; set; }
-    public int Price { get; set; }
-    private int Quantity { get; set; }
-    public bool LowStockAlert { get; set; }
-    
+    private List<Product> products;
+
+    public Inventory()
+    {
+        products = new List<Product>();
+    }
 }
