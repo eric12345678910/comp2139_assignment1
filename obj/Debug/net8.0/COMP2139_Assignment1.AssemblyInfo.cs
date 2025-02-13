32ddf63e0ae75ae39d6f6cdcc654858116f1f6b3
eric12345678910 <ericlaudrum@gmail.com>
2025-02-13 11:50:12 -0500

    Add Category option to Add Product form

--- obj/Debug/net8.0/COMP2139_Assignment1.AssemblyInfo.cs
+++ obj/Debug/net8.0/COMP2139_Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139_Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efed3edb89567f4ca1df190653f9d20ffb72806a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93032d4fa798056c45531cafe1ed022c5e28ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139_Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139_Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
